09c9deb1509828d6755a87b19a993e9a65e3748d
Lea <maroltso@grinnell.edu>
2012-10-24 16:41:32 -0500

    Initial Commit

---
+++ KDIC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KDIC
+//
+//  Created by Lea Marolt on 10/24/12.
+//  Copyright (c) 2012 Lea Marolt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ KDIC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KDIC
+//
+//  Created by Lea Marolt on 10/24/12.
+//  Copyright (c) 2012 Lea Marolt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
