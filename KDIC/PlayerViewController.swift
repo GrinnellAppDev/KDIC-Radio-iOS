92ce5b359080c4475d16ff712299f51e5098d499
ahinchman1 <hinchman17@grinnell.edu>
2017-04-09 12:24:04 -0500

    added image data sets

--- KDIC/PlayerViewController.swift
+++ KDIC/PlayerViewController.swift
@@ -13,7 +13,16 @@ class PlayerViewController: UIViewController {
     
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     self.imageView.image = UIImage(named: "Equalizer")
+    
+    button.setImage(imgShell1, forState:.Normal);
+    button.setImage(coin, forState:.Highlighted);
+    
+    button.setBackgroundImage(resizeImageWithAspect(UIImage(data: data!)!,size:button.bounds.size), forState: .Normal);
+    button.contentMode = UIViewContentMode.ScaleAspectFit;
+    button.clipsToBounds = true;
+    
   }
     
   override func viewDidAppear(_ animated: Bool) {
@@ -22,5 +31,33 @@ class PlayerViewController: UIViewController {
     KDICPlayer.play()
     print("Playing!")
   }
+    
+    func resizeImageWithAspect(image: UIImage,size: CGSize)->UIImage
+    {
+        let oldWidth = image.size.width;
+        let oldHeight = image.size.height;
+        
+        let scaleFactor = (oldWidth > oldHeight) ? size.width / oldWidth : size.height / oldHeight;
+        
+        let newHeight = oldHeight * scaleFactor;
+        let newWidth = oldWidth * scaleFactor;
+        let newSize = CGSizeMake(newWidth, newHeight);
+        
+        if UIScreen.mainScreen().respondsToSelector(#selector(NSDecimalNumberBehaviors.scale)){
+            UIGraphicsBeginImageContextWithOptions(newSize,false,UIScreen.mainScreen().scale);
+        }
+        else
+        {
+            UIGraphicsBeginImageContext(newSize);
+        }
+        
+        image.drawInRect(CGRectMake(0, 0, newSize.width, size.height));
+        let newImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        return newImage;
+    }
+    
+    
 
 }
