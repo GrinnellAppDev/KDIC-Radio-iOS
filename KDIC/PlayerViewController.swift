874144472ffbbac97b786108c6d6bad2d637f206
ahinchman1 <hinchman17@grinnell.edu>
2017-04-26 18:42:41 -0500

    Fix errors and update storyboard

--- KDIC/PlayerViewController.swift
+++ KDIC/PlayerViewController.swift
@@ -7,6 +7,7 @@ class PlayerViewController: UIViewController {
   @IBOutlet weak var subtitleLabel: UILabel!
   @IBOutlet weak var imageView: UIImageView!
 
+    @IBOutlet weak var button: UIButton!
   @IBAction func togglePlayerButtonWasTapped(_ sender: Any) {
     KDICPlayer.toggle()
   }
@@ -14,50 +15,14 @@ class PlayerViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    self.imageView.image = UIImage(named: "Equalizer")
-    
-    button.setImage(imgShell1, forState:.Normal);
-    button.setImage(coin, forState:.Highlighted);
-    
-    button.setBackgroundImage(resizeImageWithAspect(UIImage(data: data!)!,size:button.bounds.size), forState: .Normal);
-    button.contentMode = UIViewContentMode.ScaleAspectFit;
-    button.clipsToBounds = true;
-    
   }
     
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
     print("About to play")
-    KDICPlayer.play()
+    KDICPlayer.live()
     print("Playing!")
   }
-    
-    func resizeImageWithAspect(image: UIImage,size: CGSize)->UIImage
-    {
-        let oldWidth = image.size.width;
-        let oldHeight = image.size.height;
-        
-        let scaleFactor = (oldWidth > oldHeight) ? size.width / oldWidth : size.height / oldHeight;
-        
-        let newHeight = oldHeight * scaleFactor;
-        let newWidth = oldWidth * scaleFactor;
-        let newSize = CGSizeMake(newWidth, newHeight);
-        
-        if UIScreen.mainScreen().respondsToSelector(#selector(NSDecimalNumberBehaviors.scale)){
-            UIGraphicsBeginImageContextWithOptions(newSize,false,UIScreen.mainScreen().scale);
-        }
-        else
-        {
-            UIGraphicsBeginImageContext(newSize);
-        }
-        
-        image.drawInRect(CGRectMake(0, 0, newSize.width, size.height));
-        let newImage = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        
-        return newImage;
-    }
-    
     
 
 }
